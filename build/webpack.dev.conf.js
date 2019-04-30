fab71369e31ad1c513723e35064101b16bda99e8
Edward <koo22952@gmail.com>
2019-05-01 03:21:40 +0800

    dist

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -61,8 +61,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true,
-      favicon: './static/favicon.ico'
+      inject: true
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
