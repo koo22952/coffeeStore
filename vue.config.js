9b44479cab438472050479b94d70a71e214f5288
Edward <koo22952@gmail.com>
2019-05-01 00:45:37 +0800

    baseUrl

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  lintOnSave: false,
+  baseUrl: 'coffee/dist',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
